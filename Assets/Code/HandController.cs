0c78f4c6c41ad4f0284f829db9ef75f0448d73bb
Strubloid <strubloid@gmail.com>
2026-01-05 16:53:07 +0000

    starting the hand helding cards

--- Assets/Code/HandController.cs
+++ Assets/Code/HandController.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HandController : MonoBehaviour
@@ -7,6 +9,9 @@ public class HandController : MonoBehaviour
 
     // Min and max position of the hand
     public Transform minPos, maxPos;
+
+    // This is a list of vectors that will be holding the card positions in the hand
+    public List<Vector3> cardPositions = new List<Vector3>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,6 +30,30 @@ public class HandController : MonoBehaviour
      */
     public void SetCardPositionsInHand() 
     {
-    
+        cardPositions.Clear();
+
+        Vector3 distanceBetweenPoints = Vector3.zero;
+
+        // Checking if we have more than one card
+        if (heldCards.Length > 1) {
+            distanceBetweenPoints = (maxPos.position - minPos.position) / (heldCards.Length - 1);
+        }
+
+        // for loop that will be setting the card positions in the hand
+        for (int i = 0; i < heldCards.Length; i++) 
+        {
+            Vector3 cardPos = minPos.position + (distanceBetweenPoints * i);
+            cardPositions.Add(cardPos);
+
+            // now we set the card position to the calculated position
+            heldCards[i].transform.position = cardPositions[i];
+        }
+
+        //for (int i = 0; i < heldCards.Length; i++) 
+        //{
+        //    float t = heldCards.Length == 1 ? 0.5f : (float)i / (heldCards.Length - 1);
+        //    Vector3 cardPos = Vector3.Lerp(minPos.position, maxPos.position, t);
+        //    cardPositions.Add(cardPos);
+        //}
     }
 }
