fa94d74e2ff0712315675692260c1d61ea331d3a
Strubloid <strubloid@gmail.com>
2026-01-27 17:33:48 +0000

    adding the card point place for enemy and player

---
+++ Assets/Code/Cards/CardPlacePointEnemy.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.XR;
+
+public class CardPlacePointEnemy : MonoBehaviourWithMouseControls
+{
+    /**
+     * Awake is called when the script instance is being loaded
+     * so we can initialize the sprite renderer and set the base color
+     **/
+    private void Awake()
+    {
+               
+    }
+
+    /**
+     * This will be called when the mouse hover enters the card
+     */
+    protected override void OnHoverEnter()
+    {
+
+        
+    }
+
+    /**
+     * This will be called when the mouse hover exits the card
+     */
+    protected override void OnHoverExit()
+    {
+        
+
+    }
+
+}
